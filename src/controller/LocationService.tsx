6f1d50ef42ed8b026a3ad4e3812bec3cc10b4378
hamed435 <eng.mahmoud220@gmail.com>
2024-02-15 10:45:08 +0200

    get current location to save it with request data

--- src/controller/LocationService.tsx
+++ src/controller/LocationService.tsx
@@ -1,18 +1,11 @@
 import Geolocation from '@react-native-community/geolocation';
 import { PermissionsAndroid, Platform } from 'react-native';
 
-
-
-
-
-
+///////===== Request Location Permission 
 export const requestLocationPermission = async () => {
     return new Promise(async (resolved, rejected) => {
         if (Platform.OS === 'ios') {
-
             resolved(getOneTimeLocation)
-
-
         } else {
             try {
                 const granted = await PermissionsAndroid.request(
@@ -22,44 +15,37 @@ export const requestLocationPermission = async () => {
                         message: 'This App needs to Access your location',
                     },
                 );
-                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                    //To Check, If Permission is granted
-
+                if (granted === PermissionsAndroid.RESULTS.GRANTED) { //To Check, If Permission is granted
                     resolved(getOneTimeLocation)
-                    //subscribeLocationLocation();
                 } else {
                     rejected('Permission Denied')
-                    //setLocationStatus('Permission Denied');
                 }
             } catch (err) {
                 rejected(err)
                 console.warn(err);
             }
         }
-
     })
 }
 
 
-
+///////===== Request Current Location  
 export const getOneTimeLocation = async () => {
     console.log('location P')
     return new Promise((resolved, rejected) => {
         Geolocation.getCurrentPosition(
-            //Will give you the current location
-            (position) => {
+            (position) => {  //Will give you the current location
                 console.log('location success')
                 resolved(position)
             },
             (error) => {
                 console.log('location err')
                 rejected(error)
-                // setLocationStatus(error.message);
             },
             {
                 enableHighAccuracy: false,
                 timeout: 30000,
-               // maximumAge: 1000
+                // maximumAge: 1000
             },
         )
     })
